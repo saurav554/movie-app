573d39e3bc9aa42cf2d6b6029e10c6b01c04637b
saurav <avisaurav485@gmail.com>
2021-07-15 11:01:41 +0530

    Updates

---
+++ precache-manifest.9325f59b29f7e779c591bf8a963ac10d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb02e801e2f8445ec0cf9d22752f0c4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "36db5851528f9136d15d",
+    "url": "/my-app/static/css/2.0a605641.chunk.css"
+  },
+  {
+    "revision": "41998f44ea6f036483d1",
+    "url": "/my-app/static/css/main.9589140c.chunk.css"
+  },
+  {
+    "revision": "36db5851528f9136d15d",
+    "url": "/my-app/static/js/2.95bcbc73.chunk.js"
+  },
+  {
+    "revision": "41998f44ea6f036483d1",
+    "url": "/my-app/static/js/main.49999626.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
